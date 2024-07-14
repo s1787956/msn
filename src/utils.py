b75aa1831dd0693a8d0a06e873abff0cc73e1a83
s1787956 <tim.lenz@tu-dresden.de>
2024-07-14 18:00:45 +0200

    added env init method for distributed init.

--- src/utils.py
+++ src/utils.py
@@ -55,6 +55,7 @@ def init_distributed(port=40111, rank_and_world_size=(None, None)):
         #os.environ['MASTER_PORT'] = str(port)
         torch.distributed.init_process_group(
             backend='nccl',
+            init_method="env://",
             world_size=world_size,
             rank=rank)
     except Exception:
