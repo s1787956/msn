cd5b5d7d6cafdbd2fdcf490f9c5451660e494bce
lenz5 <lenz5@jwlogin22.juwels>
2024-07-14 17:43:34 +0200

    juelich multinode specialties require some adjustments..

--- src/utils.py
+++ src/utils.py
@@ -39,13 +39,13 @@ def init_distributed(port=40111, rank_and_world_size=(None, None)):
         return dist.get_world_size(), dist.get_rank()
 
     rank, world_size = rank_and_world_size
-    os.environ['MASTER_ADDR'] = 'localhost'
+    #os.environ['MASTER_ADDR'] = 'localhost'
 
     if (rank is None) or (world_size is None):
         try:
             world_size = int(os.environ['SLURM_NTASKS'])
             rank = int(os.environ['SLURM_PROCID'])
-            os.environ['MASTER_ADDR'] = os.environ['HOSTNAME']
+            #os.environ['MASTER_ADDR'] = os.environ['HOSTNAME']
         except Exception:
             logger.info('SLURM vars not set (distributed training not available)')
             world_size, rank = 1, 0
